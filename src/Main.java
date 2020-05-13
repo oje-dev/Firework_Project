d28c30e0524d7e766d96714090aeefd4e115ecfe
Oliver El-kheir <44790306+oje-dev@users.noreply.github.com>
2020-05-13 02:39:34 +0100

    Added code to create a new main panel.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import UI.Panel;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
-
+        Panel mainWindow = new Panel(1400, 800, "BBB Firework Timing Application");
+        mainWindow.generateMainPanel();
     }
 }
